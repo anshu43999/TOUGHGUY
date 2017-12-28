907d71da776fcebc25ae89abd3c2a3a47c230dc6
bloodkey <3028336816@qq.com>
2017-12-28 10:13:20 +0800

    分页第二版

--- assets/scripts/product/product.js
+++ assets/scripts/product/product.js
@@ -1,3 +1,13 @@
 $(function(){
-	console.log('product')
+	$('.pagenav').lemonPaging({
+   	'url':"page.php",//ajax请求地址
+   	'total':217,//默认数据在非ajax本地时生效的总数据条数
+   	'page_size':10,//每页数据条数
+   	'pages':7,//分页可显示页码数量
+   	'pre_next':'false',//默认显示上一页下一页
+   	'searchable':'false',//默认带跳转输入框
+   	'successcallback':function(data){
+   		console.log(111)
+   	},//列表数据填充容器
+   });
 })
