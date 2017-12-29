1619d4d82a783c83b234f6513968b135f369d33b
bloodkey <3028336816@qq.com>
2017-12-29 09:49:28 +0800

    修改修改

--- assets/scripts/product/product.js
+++ assets/scripts/product/product.js
@@ -1,13 +1,20 @@
 $(function(){
+	$(".res_con_right_title_guide_select li").on("click",function(){
+		$(".res_con_right_title_guide_select li").removeClass("active")
+		$(this).addClass("active")
+		$(".res_con_right_title_guide_select_content li").removeClass("active")
+		$(".res_con_right_title_guide_select_content li").eq($(this).index()).addClass("active")
+	})
+	
 	$('.pagenav').lemonPaging({
-   	'url':"page.php",//ajax请求地址
-   	'total':217,//默认数据在非ajax本地时生效的总数据条数
-   	'page_size':10,//每页数据条数
-   	'pages':7,//分页可显示页码数量
-   	'pre_next':'false',//默认显示上一页下一页
-   	'searchable':'false',//默认带跳转输入框
-   	'successcallback':function(data){
-   		console.log(111)
-   	},//列表数据填充容器
-   });
+	   	'url':"page.php",//ajax请求地址
+	   	'total':217,//默认数据在非ajax本地时生效的总数据条数
+	   	'page_size':10,//每页数据条数
+	   	'pages':7,//分页可显示页码数量
+	   	'pre_next':'false',//默认显示上一页下一页
+	   	'searchable':'false',//默认带跳转输入框
+	   	'successcallback':function(data){
+	   		console.log(111)
+	   	},//列表数据填充容器
+    });
 })
